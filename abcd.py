e68f8377816c222b12fc3a7c77b7bb6b9022d5ef
saurabh <saurabh956@gmail.com>
2020-03-21 14:12:15 +0530

    mybranch commit

--- abcd.py
+++ abcd.py
@@ -4,7 +4,4 @@ sen1 = '''hi this is saurabh verma, i am the great'''
 sen2 = 'start a sentence and the bring it to end'
 sen3 = 'you are the great'
 pattern = re.compile(r'verma')
-matches = pattern.finditer(sen1)
-print('saurabh')
-print(sen1)
-print(sen2)
+
