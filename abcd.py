836d2ab55d9177a7a49c6754cf49972ae178bf87
saurabh <saurabh956@gmail.com>
2020-03-21 13:47:37 +0530

    new commit

--- abcd.py
+++ abcd.py
@@ -2,6 +2,7 @@ import re
 
 sen1 = '''hi this is saurabh verma, i am the great'''
 sen2 = 'start a sentence and the bring it to end'
+sen3 = 'you are the great'
 pattern = re.compile(r'verma')
 matches = pattern.finditer(sen1)
 print('saurabh')
